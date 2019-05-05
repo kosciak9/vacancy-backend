5b519f84de5431255af4c0733fa6e62c5a8548bc
Franciszek Madej <f.madej@protonmail.com>
2019-05-05 19:20:02 +0200

    add CaptainView endpoint

--- vacancy/users/views.py
+++ vacancy/users/views.py
@@ -1,4 +1,4 @@
-from datetime import date, timedelta, time, datetime 
+from datetime import date, timedelta, time, datetime
 
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
@@ -8,8 +8,8 @@ from rest_framework.response import Response
 from vacancy.users.models import Availability, Position, Team, User
 from vacancy.users.permissions import OwnTeamOrPermissionDenied
 from vacancy.users.serializers import (AvailabilitySerializer,
-                                      PositionSerializer, TeamSerializer,
-                                      UserSerializer)
+                                       PositionSerializer, TeamSerializer,
+                                       UserSerializer)
 
 
 class AvailabilityViewSet(viewsets.ModelViewSet):
@@ -56,6 +56,41 @@ def per_user_priority_availability(request):
         priority_availability.append(day)
         iteration_date = iteration_date + timedelta(days=1)
     return Response(priority_availability)
+
+
+@api_view(['GET'])
+def team_priority_availability(request):
+    player = request.user
+    days_ahead = player.team.priority_days_ahead
+    weekday_date = date.today().weekday()
+    iteration_date = date.today() + timedelta(days=7-weekday_date)
+    start_hour = player.team.start_hour
+    interval = player.team.interval  # shadow this later
+    interval = timedelta(hours=interval.hour, minutes=interval.minute)
+    hour_count = player.team.hour_count
+    days_ahead = player.team.priority_days_ahead
+    weekday_date = date.today().weekday()
+    iteration_date = date.today() + timedelta(days=7-weekday_date)
+    team_availability = []
+    for _i in range(days_ahead):
+        iteration_time = time(hour=start_hour.hour, minute=start_hour.minute)
+        day = { 'date': iteration_date.isoformat(), 'availability': {} }
+        for _j in range(hour_count):
+            filtered_availability = list(Availability.objects.filter(
+                date=iteration_date,
+                time=iteration_time,
+                player__team=player.team
+            ))
+            day['availability'][iteration_time.isoformat()] = []
+            for availability in filtered_availability:
+                day['availability'][iteration_time.isoformat()].append(
+                    AvailabilitySerializer(availability).data
+                )
+            iteration_time = (datetime.combine(
+                date.today(), iteration_time) + interval).time()
+        team_availability.append(day)
+        iteration_date = iteration_date + timedelta(days=1)
+    return Response(team_availability)
 
 
 class TeamViewSet(viewsets.ModelViewSet):
